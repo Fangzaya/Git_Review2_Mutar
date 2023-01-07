d593b709fa7802b97a347ffac82c07cddd5183f0
Fangzaya <114306516+Fangzaya@users.noreply.github.com>
2023-01-07 14:31:41 -0500

    Update FirstClass.java

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Today is last day of Java class 1/7/2023");
         System.out.println(" :) ");
+        // add some change from GitHub
     }
 }
