1b89151de8a716b8ef677c62ecf95d2a872a5a92
Atittaya <tunaatittaya@gmail.com>
2023-01-07 14:30:57 -0500

    change 1

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,5 +3,6 @@ package firstPackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Today is last day of Java class 1/7/2023");
+        System.out.println(" :) ");
     }
 }
